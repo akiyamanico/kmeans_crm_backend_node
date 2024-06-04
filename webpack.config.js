d42b7c54e9cf58722426e365bd793ad113c292dc
Fi Zilalil Huda <76860149+akiyamanico@users.noreply.github.com>
2024-06-05 02:12:20 +0700

    a

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,6 @@
-const path = require('path');
-
 module.exports = {
   mode: 'production',
   entry: './index.js',
-  output: {
-    path: path.join(__dirname, 'dist'),
-    publicPath: '/',
-    filename: 'final.js',
-  },
   module: {
     rules: [
       {
