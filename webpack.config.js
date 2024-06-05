729cb633991e989828dfef788ca050c92d1a4bc8
Fi Zilalil Huda <76860149+akiyamanico@users.noreply.github.com>
2024-06-05 13:59:13 +0700

    a

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,30 @@
+const path = require('path');
 module.exports = {
   mode: 'production',
   entry: './index.js',
+  target: 'node',
+  resolve: {
+    extensions: ['.js', '.json', '.node'],
+    alias: {
+      'npm': false, // Ignore npm module
+      'child_process': false, // Ignore child_process module
+      'fs': false, // Ignore fs module
+      'path': false, // Ignore path module
+    },
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
+        test: /\.node$/,
+        use: 'node-loader'
+      },
+      {
+        test: /\.cs$/, // Ignore .cs files
+        use: 'ignore-loader'
       },
       {
         test: /\.html$/,
@@ -18,5 +34,4 @@ module.exports = {
       },
     ],
   },
-  target: 'node',
 };
